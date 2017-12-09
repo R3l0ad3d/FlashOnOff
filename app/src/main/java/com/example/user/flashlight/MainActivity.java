11620bef5f588a40e5ad74c9ad013608bce583e6
Joy <azizur.rahman.bu@gmail.com>
2017-12-09 12:37:26 +0600

    initial

---
+++ app/src/main/java/com/example/user/flashlight/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.user.flashlight;
+
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ToggleButton toggleButton;
+    Camera camera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleButton = findViewById(R.id.onOffFlashlight);
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                if (checked) {
+                    if((getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH))){
+                        camera = Camera.open();
+                        Camera.Parameters parameters = camera.getParameters();
+                        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                        camera.setParameters(parameters);
+                        camera.startPreview();
+                    }
+                    //ToDo something
+
+
+                } else {
+
+                    //ToDo something
+                    camera = Camera.open();
+                    Camera.Parameters parameters = camera.getParameters();
+                    parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                    camera.setParameters(parameters);
+                    camera.stopPreview();
+                    camera.release();
+
+                }
+            }
+        });
+    }
+}
